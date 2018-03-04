792fa4bcd91ffa969f3fec6aec9cc43156cfc9be
Ayushya Amitabh <ayushyamitabh@gmail.com>
2018-03-03 19:54:14 -0500

    Fix firebase credentials

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import registerServiceWorker from './registerServiceWorker';
 import 'typeface-roboto';
 import * as firebase from 'firebase';
 var config = {
-    apiKey: "AIzaSyASMSS6bIKWx14lFB23k0WCJr4_Ek06vAI",
-    authDomain: "pos-tmag.firebaseapp.com",
-    databaseURL: "https://pos-tmag.firebaseio.com",
-    projectId: "pos-tmag",
-    storageBucket: "pos-tmag.appspot.com",
-    messagingSenderId: "626586569919"
+    apiKey: "AIzaSyCKGQ8GCl0H3AD4BMpHP_VysNJJxW--np8",
+    authDomain: "pos-tagmhaxt.firebaseapp.com",
+    databaseURL: "https://pos-tagmhaxt.firebaseio.com",
+    projectId: "pos-tagmhaxt",
+    storageBucket: "",
+    messagingSenderId: "455987530569"
 };
 firebase.initializeApp(config);
 ReactDOM.render(<App />, document.getElementById('root'));
