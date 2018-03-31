99af38be2bcf68931f2bd4ea1bcf3d093e813f2e
Melvin Tham <melvintham1997@gmail.com>
2018-03-31 15:44:48 -0400

    added UI for AddShop

--- website/src/Home/AddShop/AddShop.js
+++ website/src/Home/AddShop/AddShop.js
@@ -1,12 +1,78 @@
 import React, {Component} from 'react';
 import './AddShop.css';
+import {Button,
+        Card,
+        CardContent,
+        CardHeader,
+        Divider, 
+        FormControl,
+        InputLabel,
+        IconButton, 
+        InputAdornment,
+        LinearProgress, 
+        MenuItem,
+        Select, 
+        Snackbar,
+        TextField,
+        Typography} from 'material-ui';
+import Visible from 'material-ui-icons/Visibility';
+import Hidden from 'material-ui-icons/VisibilityOff';
+import Next from 'material-ui-icons/PlayCircleFilled';
+import FileUpload from 'material-ui-icons/CloudUpload';
+import Done from 'material-ui-icons/CheckCircle';
+import * as firebase from 'firebase';
+
 
 export default class AddShop extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    };
+    
+    
     render() {
-        return(
-            <div>
-                Add Shop Page
+        return(  
+             
+           
+            <div className="addShop-page">   
+                <div className="addShop-title-bar">
+                    <Typography variant="display2">
+                       Weirdoughs | Add-Shop
+                    </Typography>
+                    <Typography variant="subheading" style={{marginTop:'10px'}}>
+                        Add a new shop below!
+                    </Typography>
+                </div>       
+                <Divider />
+            
+           
+             <Typography style={{marginTop:'10px'}} variant="subheading"> 
+                <Card style={{marginTop:'10px'}} data-aos="slide-up">
+                 <CardContent>
+            <TextField                             
+                label="Shop"
+                type="Shop"
+                id="Shop"
+                required
+                fullWidth
+                className="push-down"
+                                />
+            <Button fullWidth 
+                variant="raised" 
+                color="primary" 
+                className="push-down">
+                Go
+            </Button>
+            
+            </CardContent>
+            </Card>
+            </Typography>
+          
+      
             </div>
+            
         );
     }
 }
