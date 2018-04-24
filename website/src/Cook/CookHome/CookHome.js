2d190950dbca70ff6e6f7eca2bde93a8d145c5dd
Gerry Xu <gerryxu.zqx@gmail.com>
2018-04-24 11:59:42 -0400

    Testing adding pizza choices to database

--- website/src/Cook/CookHome/CookHome.js
+++ website/src/Cook/CookHome/CookHome.js
@@ -1,19 +1,22 @@
 import React, {Component} from 'react';
 import './CookHome.css';
 import * as firebase from 'firebase';
-import {AppBar,
+import {Avatar,
+        AppBar,
         Typography,
         Toolbar,
+        Divider,
         Button} from 'material-ui';
-import { Link } from 'react-router-dom';
+import Logout from 'material-ui-icons/ExitToApp'
 
 class CookHome extends Component {
     constructor(props) {
         super(props);
         this.state = {
             cook: {
-                cookuid: '',
-                name: '',
+                cookUID: '',
+                cookName: null,
+                profilePicture: null,
                 shopUID: '',
                 pizzaID: '',
                 averageRating: '',
@@ -21,12 +24,11 @@ class CookHome extends Component {
                 WarnedCount: '',
                 warned: false,
             },
-
-            user: {
-                displayName:''
-            }
+            pizzas: []
         };
+        this.fireBaseListener = null;
         this.authListener = this.authListener.bind(this);
+
     }
 
     componentDidMount() {
@@ -34,28 +36,63 @@ class CookHome extends Component {
     }
 
     authListener() {
-        this.fireBaseListener = firebase.auth().onAuthStateChanged((user) => {
-            if(user) {
+        this.fireBaseListener = firebase.auth().onAuthStateChanged((cook) => {
+            if(cook) {
+                console.log(cook);
                 this.setState({
-                    user: {
-                        displayName: user.displayName
+                    cook: {
+                        cookName: cook.displayName,
+                        profilePicture: cook.photoURL
                     }
                 })
             }
         });
+        const rootRef = firebase.database().ref().child('Pizzas');
+        var pizzaRef = rootRef.child('pizzaID3').set({
+            name: 'Pepperoni Pizza',
+            cost: 12.99
+        });
+        pizzaRef = rootRef.child('pizzaID4').set({
+            name:'Bacon Pizza',
+            cost: 14.99
+        });
+        pizzaRef = rootRef.child('pizzaID5').set({
+            name:'Mushrooms and Onions Pizza',
+            cost: 10.99
+        });
+        pizzaRef = rootRef.child('pizzaID6').set({
+            name:'Sausage Pizza',
+            cost: 13.99
+        });
+        pizzaRef = rootRef.child('pizzaID7').set({
+            name:'Black Olives Pizza',
+            cost: 10.99
+        });
+    }
+
+    componentWillUnmount() {
+        this.fireBaseListener && this.fireBaseListener();
+        this.authListener = undefined;
     }
 
     render() {
         return (
-            <div>
-                <Typography variant="display2">
-                    Welcome, {this.state.user.displayName}
-                </Typography>
-                    <Typography variant="title" color="inherit">
-                        <Button color ="secondary" variant = "raised" style={{float: 'right'}}
-                                onClick={()=>{firebase.auth().signOut()}}> Logout
-                        </Button>
-                    </Typography>
+            <div style={{padding:'50px 100px'}}>
+                <div className="cook-header">
+                    <Avatar className="cook-avatar"
+                        src={this.state.cook.profilePicture} />
+                        <Typography variant="display2" style={{flex:1}}>
+                            Welcome, {this.state.cook.cookName}
+                            <Button style={{float:'right'}} onClick={()=>{firebase.auth().signOut()}}>
+                            <Logout style ={{marginRight: '5px'}}/> Logout
+                            </Button>
+                        </Typography>
+                </div>
+                <Divider />
+                <div>
+
+
+                </div>
             </div>
         );
     }
